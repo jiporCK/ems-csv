a0e1006593cb04832de8afb9ff2924136a331284
jiporCK <srengchipor99@gmail.com>
2025-07-25 16:58:02 +0700

    second commit

--- src/EmployeeManager.java
+++ src/EmployeeManager.java
@@ -1,6 +1,4 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLSyntaxErrorException;
@@ -45,9 +43,42 @@ public class EmployeeManager {
     }
 
     private static void searchEmployeeId() {
+
+        System.out.print("Enter an id: ");
+        Integer id = Integer.parseInt(scanner.nextLine());
+
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(path.toFile())
+        )) {
+            String value;
+            while ((value = reader.readLine()) != null) {
+                Employee employee = Employee.fromCSV(value);
+                if (employee.getId().equals(id)) {
+                    System.out.println(employee);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     private static void listAllEmployees() {
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader(path.toFile())
+        )) {
+
+            String value;
+            while ((value = reader.readLine()) != null) {
+                Employee employee = Employee.fromCSV(value);
+                System.out.println(employee);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
     private static void addEmployee() {
@@ -78,6 +109,5 @@ public class EmployeeManager {
             System.out.println(e.getMessage());
         }
     }
-
 
 }
---
+++ src/JsonConverter.java
@@ -0,0 +1,45 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonConverter {
+
+    public static void main(String[] args) throws IOException {
+
+//        Employee employee = new Employee(1,"koko",300.0,"IT" );
+
+        List<Employee> employees = new ArrayList<>() {{
+            add(new Employee(1, "tho", 3000.0, "Smash"));
+            add(new Employee(2, "hulk", 300.0, "Smash"));
+            add(new Employee(3, "ironman", 10000.0, "AI"));
+        }};
+
+        Path path = Paths.get("src/employee.json");
+
+        ObjectMapper mapper = new ObjectMapper();
+
+//        mapper.writeValue(path.toFile(), employees);
+
+//        List<Employee> employeeList = mapper.readValue(path.toFile(),
+//                new TypeReference<>() {
+//                });
+//
+//        employeeList.forEach(System.out::println);
+
+        JsonNode jsonNode = mapper.readTree(path.toFile());
+        ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+
+        String prettyJson = writer.writeValueAsString(jsonNode);
+
+        System.out.println(prettyJson);
+
+    }
+
+}
